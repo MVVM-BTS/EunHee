c0d5feffc388e48cb94f7ca59a33b3176af71332
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-09-18 01:02:24 +0900

    [#3] 계층 구조 잡기

---
+++ Clean-Architecture/Clean-Architecture/Source/Domain/UseCases/FetchRecentMovieQueriesUseCase.swift
@@ -0,0 +1,8 @@
+//
+//  FetchRecentMovieQueriesUseCase.swift
+//  Clean-Architecture
+//
+//  Created by 정은희 on 2022/09/18.
+//
+
+import Foundation
---
+++ Clean-Architecture/Clean-Architecture/Source/Presentations/MovieScene/MovieList/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clean-Architecture
+//
+//  Created by 정은희 on 2022/09/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
