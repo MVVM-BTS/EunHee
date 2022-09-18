06fa4f9d5a79db214b10d270debd6e75e255bc0c
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-09-18 18:31:05 +0900

    [#3] Domain Layer, Presentation Layer

--- Clean-Architecture/Clean-Architecture/Source/Presentations/MovieScene/MovieList/View/ViewController.swift
+++ Clean-Architecture/Clean-Architecture/Source/Presentations/MovieScene/MovieList/View/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import RxSwift
 
-class ViewController: UIViewController {
+final class MoviesListViewController: UIViewController, StoryboardInstantiable, UISearchBarDelegate {
+    
+    private var viewModel: MoviesListViewModel!
+    
+    final class func create(with viewModel: MoviesListViewModel) -> MoviesListViewController {
+        let vc = MoviesListViewController.instantiateViewController()
+        vc.viewModel = viewModel
+        return vc
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        bind(to: viewModel)
     }
 
-
+    private func bind(to viewModel: MoviesListViewModel) {
+        viewModel.items.observe(on: self) { [weak self] items in
+            self?.moviesTableViewController?.items = items
+        }
+        viewModel.error.observe(on: self) { [weak self] error in
+            self.showError(error)
+        }
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchText = searchBar.text, !searchText.isEmpty else { return }
+        viewModel.didSearch(query: searchText)
+    }
 }
 
