5b028be350c229b59bcb64eeb358944566f2674e
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-10-04 22:27:50 +0900

    [#3] Data Layer, Network Layer

--- Clean-Architecture/Clean-Architecture/Source/Data/Network/APIEndPoints.swift
+++ Clean-Architecture/Clean-Architecture/Source/Data/Network/APIEndPoints.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+// Infra Layer (= Network Layer)
+
+struct APIEndpoints {
+    
+    static func getMovies(with moviesRequestDTO: MoviesRequestDTO) -> Endpoint<MoviesResponseDTO> {
+
+        return Endpoint(path: "search/movie/",
+                        method: .get,
+                        queryParametersEncodable: moviesRequestDTO)
+    }
+}
+
+
+let config = ApiDataNetworkConfig(baseURL: URL(string: appConfigurations.apiBaseURL)!,
+                                  queryParameters: ["api_key": appConfigurations.apiKey])
+let apiDataNetwork = DefaultNetworkService(session: URLSession.shared,
+                                           config: config)
+
+let endpoint = APIEndpoints.getMovies(with: MoviesRequestDTO(query: query.query,
+                                                             page: page))
+dataTransferService.request(with: endpoint) { (response: Result<MoviesResponseDTO, Error>) in
+    let moviesPage = try? response.get()
+}
--- Clean-Architecture/Clean-Architecture/Source/Data/Repositories/DetailMoviesRepository.swift
+++ Clean-Architecture/Clean-Architecture/Source/Data/Repositories/DetailMoviesRepository.swift
@@ -6,3 +6,57 @@
 //
 
 import Foundation
+
+// DataLayer
+
+final class DetailMoviesRepository {
+    
+    private let dataTransferService: DataTransfer
+    
+    init(dataTransferService: DataTransfer) {
+        self.dataTransferService = dataTransferService
+    }
+}
+
+extension DetailMoviesRepository: MoviesRepository {
+    public func fetchMoviesList(query: MoviesQuery, page: Int, completion: @escaping (Result<MoviesPage, Error>) -> Void) -> Cancelable? {
+        
+        let endpoint = APIEndpoints.getMovies(with: MoviesRequestDTD(query: query.query,
+                                                                     page: page))
+        return dataTransferService.request(with: endpoint) { (response Result<MoviesResponseDTO, Error>) in
+            switch response {
+            case .success(let moviesResponseDTD):
+                completion(.success(moviesResponseDTD.toDomain()))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
+
+// MARK: DTO (Data Transfer Object)
+// DataTransferService 내부에서 JSON Response를 도메인으로 인코딩/디코딩하기 위한 중간 객체로 사용
+struct MoviesRequestDTO: Encodable {
+    let query: String
+    let page: Int
+}
+
+struct MoviesResponseDTO: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case page
+        case totalPages = "total_pages"
+        case movies = "results"
+    }
+    let page: Int
+    let totalPages: Int
+    let movies: [MovieDTO]
+}
+
+// MARK: Mappings to Domain
+extension MoviesResponseDTO {
+    func toDomain() -> MoviesPage {
+        return .init(page: page,
+                     totalPages: totalPages,
+                     movies: movies.map { $0.toDomain() })
+    }
+}
